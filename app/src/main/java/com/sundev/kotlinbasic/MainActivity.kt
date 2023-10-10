a70caa86b9e48b9efb6af913acd739bac84b9580
Hanura Diarjo <37662296+hanura13@users.noreply.github.com>
2023-10-10 23:06:14 +0700

    intro debugging

--- app/src/main/java/com/sundev/kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/sundev/kotlinbasic/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.sundev.kotlinbasic
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
+
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "this is where the app crashed before")
+        val helloworldText: TextView = findViewById(R.id.helloworld)
+        Log.d(TAG, "this should be logged if the bug is fixed")
+        helloworldText.text = "Hello, debugging"
+        logging()
+        division()
+    }
+
+    fun division(){
+        val numerator = 60
+        var denominator = 4
+        repeat(4){
+            Log.d(TAG, "$denominator")
+            Log.v(TAG, "${numerator / denominator}")
+            denominator--
+        }
+    }
+
+    fun logging(){
+        Log.e(TAG, "ERROR: a serious error like an app crash")
+        Log.w(TAG, "WARN: warns about the potential for serious error")
+        Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+        Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+        Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
     }
 }
